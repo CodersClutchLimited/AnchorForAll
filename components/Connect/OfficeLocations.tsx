0b41c57b5e43c35c9aa9da84ab4ccc0e6f0813ff
kebzin <kebbawaiga@gmail.com>
2025-02-10 18:10:37 +0000

    refactor: improve sorting and formatting in NewsFilter component

--- components/Connect/OfficeLocations.tsx
+++ components/Connect/OfficeLocations.tsx
@@ -1,96 +1,165 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const OfficeLocations = () => {
   return (
     <div className="bg-[#F8FAFC] p-6 py-20 pb-[150px]">
       <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mx-[9%]">
-        
         {/* Gambia */}
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_The_Gambia.svg/800px-Flag_of_The_Gambia.svg.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">The Gambia</h3>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_The_Gambia.svg/800px-Flag_of_The_Gambia.svg.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            The Gambia
+          </h3>
           <p className="text-[#94A3B8] text-lg ">25 Kairaba Avenue KSMD</p>
           <p className="text-[#94A3B8] text-lg ">Serrekunda Banjul, Gambia</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+220 7762666</span><br />
+            <span className="text-[#64748B] text-lg">+220 7762666</span>
+            <br />
             <span className="text-[#64748B] text-lg">+220 9972666</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://cdn.britannica.com/70/5070-050-8968E7E7/Flag-Senegal.jpg" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Senegal</h3>
+          <Image
+            src="https://cdn.britannica.com/70/5070-050-8968E7E7/Flag-Senegal.jpg"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Senegal
+          </h3>
           <p className="text-[#94A3B8] text-lg ">Cite Soprime extension</p>
           <p className="text-[#94A3B8] text-lg ">des Parcelles Assainies </p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+221 338358429</span><br />
+            <span className="text-[#64748B] text-lg">+221 338358429</span>
+            <br />
             <span className="text-[#64748B] text-lg">+221 771358271</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Côte d'Ivoire</h3>
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Côte d&apos;Ivoire
+          </h3>
           <p className="text-[#94A3B8] text-lg ">Adjame Marche Furum, </p>
-          <p className="text-[#94A3B8] text-lg ">Abidjan, Côte d'Ivoire</p>
+          <p className="text-[#94A3B8] text-lg ">Abidjan, Côte d&apos;Ivoire</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+225 70909033</span><br />
+            <span className="text-[#64748B] text-lg">+225 70909033</span>
+            <br />
             <span className="text-[#64748B] text-lg">+225 70909033</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://framerusercontent.com/images/E15Cn2kxA9NL66peOyPLBBWU.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Guinea Conakry</h3>
+          <Image
+            src="https://framerusercontent.com/images/E15Cn2kxA9NL66peOyPLBBWU.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Guinea Conakry
+          </h3>
           <p className="text-[#94A3B8] text-lg ">Commune de Ratome </p>
           <p className="text-[#94A3B8] text-lg ">T8 Carrefour Pharmacie</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+224 621796232</span><br />
+            <span className="text-[#64748B] text-lg">+224 621796232</span>
+            <br />
             <span className="text-[#64748B] text-lg">+224 622973266</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 rounded-xl hover:shadow-md transition cursor-pointer duration-300 py-4 px-6">
-          <Image src="https://framerusercontent.com/images/17IjrFmXlJvzrSxzBMpE4tTmg8.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Sierra Leone</h3>
+          <Image
+            src="https://framerusercontent.com/images/17IjrFmXlJvzrSxzBMpE4tTmg8.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Sierra Leone
+          </h3>
           <p className="text-[#94A3B8] text-lg ">No.49 Circular Road, </p>
           <p className="text-[#94A3B8] text-lg ">Freetown, Sierra Leone</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+232 99718080</span><br />
+            <span className="text-[#64748B] text-lg">+232 99718080</span>
+            <br />
             <span className="text-[#64748B] text-lg">+232 75894577</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://framerusercontent.com/images/kB99X1Ji1PnoZZkvkwr3vcUflsM.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Liberia</h3>
+          <Image
+            src="https://framerusercontent.com/images/kB99X1Ji1PnoZZkvkwr3vcUflsM.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Liberia
+          </h3>
           <p className="text-[#94A3B8] text-lg ">Lynch street (H.Lafayette)</p>
           <p className="text-[#94A3B8] text-lg ">Monrovia</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+231 55 589 5996</span><br />
+            <span className="text-[#64748B] text-lg">+231 55 589 5996</span>
+            <br />
             <span className="text-[#64748B] text-lg">+231 55 589 5996</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://framerusercontent.com/images/uvDXjtVny123UPhqGcYZORZXbo.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
+          <Image
+            src="https://framerusercontent.com/images/uvDXjtVny123UPhqGcYZORZXbo.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
           <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Mali</h3>
           <p className="text-[#94A3B8] text-lg ">Lafiabougou Rond</p>
           <p className="text-[#94A3B8] text-lg ">Cabral, Mali, Bamako</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+223 68166327</span><br />
+            <span className="text-[#64748B] text-lg">+223 68166327</span>
+            <br />
             <span className="text-[#64748B] text-lg">+223 68166327</span>
           </p>
         </div>
 
         <div className="border bg-white border-gray-200 hover:shadow-md transition duration-300 cursor-pointer rounded-xl py-4 px-6">
-          <Image src="https://framerusercontent.com/images/I05b62ZydF6Pd8wx7W3pyzUU.png" alt="Gambia Flag" width={40} height={40} className="rounded-[4px] mb-2 mt-1" />
-          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">Cameroon</h3>
+          <Image
+            src="https://framerusercontent.com/images/I05b62ZydF6Pd8wx7W3pyzUU.png"
+            alt="Gambia Flag"
+            width={40}
+            height={40}
+            className="rounded-[4px] mb-2 mt-1"
+          />
+          <h3 className="text-2xl mb-10 font-semibold text-[#282781]">
+            Cameroon
+          </h3>
           <p className="text-[#94A3B8] text-lg ">Quartier: Essos Carrefour</p>
           <p className="text-[#94A3B8] text-lg ">Avenue Germain</p>
           <p className="mt-5">
-            <span className="text-[#64748B] text-lg">+237 655737037</span><br />
+            <span className="text-[#64748B] text-lg">+237 655737037</span>
+            <br />
             <span className="text-[#64748B] text-lg">+237 651695846</span>
           </p>
         </div>
